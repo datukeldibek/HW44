6445719011076bca04d78a1367d09c58837bd213
Datkayim <dkeldibekova08@gmail.com>
2023-04-07 09:23:51 +0600

    Initial Commit

---
+++ HW44/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW44
+//
+//  Created by Jarae on 7/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
